002bd2ac493ade3420f6038ca94e3b9f01513c04
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2021-11-05 07:30:54 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,12 @@
-function
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyASFYzq7cB85fBubflsdIrrLhPGCWnT88s",
+    authDomain: "letschatapp-89335.firebaseapp.com",
+    projectId: "letschatapp-89335",
+    storageBucket: "letschatapp-89335.appspot.com",
+    messagingSenderId: "856994190131",
+    appId: "1:856994190131:web:b0b72f49e742c4c8042446"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
