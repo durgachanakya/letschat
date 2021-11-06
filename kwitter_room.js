de3f888dd9a4bbdf2ea3a371c130cd1b5ca94cfb
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2021-11-06 20:33:41 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,12 +1,18 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyASFYzq7cB85fBubflsdIrrLhPGCWnT88s",
-    authDomain: "letschatapp-89335.firebaseapp.com",
-    projectId: "letschatapp-89335",
-    storageBucket: "letschatapp-89335.appspot.com",
-    messagingSenderId: "856994190131",
-    appId: "1:856994190131:web:b0b72f49e742c4c8042446"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+var firebaseConfig = {
+  apiKey: "AIzaSyASFYzq7cB85fBubflsdIrrLhPGCWnT88s",
+  authDomain: "letschatapp-89335.firebaseapp.com",
+  databaseURL: "https://letschatapp-89335-default-rtdb.firebaseio.com",
+  projectId: "letschatapp-89335",
+  storageBucket: "letschatapp-89335.appspot.com",
+  messagingSenderId: "856994190131",
+  appId: "1:856994190131:web:b0b72f49e742c4c8042446"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
